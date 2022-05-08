db30f27015fc1112ba8a9120e6685d88a733cd06
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-08 17:32:06 +0200

    first commit

---
+++ Palidroma/main.js
@@ -0,0 +1,39 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+
+// *** function ***
+
+function palindroma(imput){
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+        console.log(reverseWord)
+        if( reverseWord === word){
+            console.log("è una parola palindroma");
+    }
+}
+}
+
+
+ const word = prompt("inserire una parola");
+
+ let reverseWord = ""
+
+if (word.length %2 === 0){
+    palindroma(word);
+}else
+{
+    console.log( word, "non è una parola palindroma")
+}
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         reverseWord += word[i];
+//         console.log(reverseWord)
+//         if( reverseWord === word){
+//             console.log("è palindroma");
+//     }
+
+// }    
+// 
---
+++ Pari e Dispari/main.js
@@ -0,0 +1,28 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// chiedere all'utente di scegliere pari o dispari
+
+// variabile booleana vero / falso 
+
+// chiedere numero finché non entra nel range da 1 a 5
+
+// il computer genera randomicamente un numero e lo salva 
+
+// sommare le due variabili 
+
+// funzione pari dispari richiamata
+
+// se la variabile del giocatore è uguale all'esito della funzione
+
+// hai vinto
+
+// else hai perso
+
